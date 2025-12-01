61118212c61b45c30b0b2779aca9e9cfcfdcdd4d
Zydryx <zydryx125@gmail.com>
2025-12-01 11:52:22 +0800

    Remove docstring from BaseTab class
    Removed docstring from BaseTab class.

--- tabs/base_tab.py
+++ tabs/base_tab.py
@@ -1,8 +1,6 @@
 import customtkinter as ctk
-from rental_system import VehicleRentalService, Vehicle, Customer, Documentation, MaintenanceRecord
 
 class BaseTab(ctk.CTkFrame):
-    """Base class for all tab frames to inherit common properties."""
     def __init__(self, master, app_controller, **kwargs):
         super().__init__(master, **kwargs)
         self.app_controller = app_controller 
